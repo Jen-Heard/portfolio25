d3be7ff07fa4c1920e7217f2cd4df73e9c5068f1
Jen-Heard <jlheard11@gmail.com>
2025-01-29 13:07:25 -0600

    Added js code for form submission

--- js/main.js
+++ js/main.js
@@ -11,15 +11,13 @@ function myFunction() {
     const sideMenuCheckbox = document.getElementById('side-menu');
     const nav = document.querySelector('.nav');
     const hambLabel = document.querySelector('.hamb');
-  
-    // Listen for clicks outside the menu
+
     document.addEventListener('click', function(event) {
       if (!nav.contains(event.target) && !sideMenuCheckbox.contains(event.target) && !hambLabel.contains(event.target)) {
         sideMenuCheckbox.checked = false;
       }
     });
   
-    // Listen for clicks on the hamburger menu icon
     hambLabel.addEventListener('click', function(event) {
       if (sideMenuCheckbox.checked) {
         sideMenuCheckbox.checked = false;
@@ -32,18 +30,41 @@ function myFunction() {
     hiddenArticles.forEach(function(article) {
       article.classList.remove('hidden1');
     });
-    document.getElementById('see-more').style.display = 'none'; // Hide the "see more" link
+    document.getElementById('see-more').style.display = 'none'; 
   }
   
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-      event.preventDefault();
+  //   document.getElementById('contact-form').addEventListener('submit', function(event) {
+  //     event.preventDefault();
   
-      const name = document.getElementById('name').value;
-      const email = document.getElementById('email').value;
-      const services = Array.from(document.querySelectorAll('input[name="services"]:checked')).map(cb => cb.value);
-      const message = document.getElementById('message').value;
+  //     const name = document.getElementById('name').value;
+  //     const email = document.getElementById('email').value;
+  //     const services = Array.from(document.querySelectorAll('input[name="services"]:checked')).map(cb => cb.value);
+  //     const message = document.getElementById('message').value;
   
-      const mailtoLink = `mailto:jenlheard@icloud.com?subject=Contact Form Submission&body=Name: ${name}%0D%0AEmail: ${email}%0D%0AServices: ${services.join(', ')}%0D%0AMessage: ${message}`;
+  //     const mailtoLink = `mailto:jenlheard@icloud.com?subject=Contact Form Submission&body=Name: ${name}%0D%0AEmail: ${email}%0D%0AServices: ${services.join(', ')}%0D%0AMessage: ${message}`;
       
-      window.location.href = mailtoLink;
-  });
+  //     window.location.href = mailtoLink;
+  // });
+
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+ 
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const services = Array.from(document.querySelectorAll('input[name="services"]:checked')).map(cb => cb.value);
+    const message = document.getElementById('message').value;
+ 
+    const templateParams = {
+       name: name,
+       email: email,
+       services: services.join(', '),
+       message: message
+    };
+ 
+    emailjs.send('service_y4rwqtp', 'template_w8xgmyi', templateParams)
+       .then(function(response) {
+          console.log('SUCCESS!', response.status, response.text);
+       }, function(error) {
+          console.log('FAILED...', error);
+       });
+ });
